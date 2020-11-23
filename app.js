4399327ae231eda4ad640718dbfd77409726ffd6
UcheSylvester <okorocode@gmail.com>
2020-11-23 01:54:15 +0100

    creating command for notes

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-const validator = require("validator");
+const chalk = require("chalk");
+const yargs = require("yargs");
+
 const getNotes = require("./notes");
 
-const notes = getNotes();
+yargs.version("1.1.0");
 
-console.log(notes);
-console.log(validator.isEmail("ucgmail.com"));
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    console.log(`title: ${argv.title}`, `body: ${argv.body}`);
+  },
+});
+
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove note",
+  handler: (args) => {
+    console.log("removing note");
+  },
+});
+
+// create list command
+yargs.command({
+  command: "list",
+  describe: "list all notes",
+  handler: (args) => {
+    console.log("listing notes");
+  },
+});
+
+// create read command
+yargs.command({
+  command: "read",
+  describe: "read note",
+  handler: (args) => {
+    console.log("reading note");
+  },
+});
+
+yargs.parse();
